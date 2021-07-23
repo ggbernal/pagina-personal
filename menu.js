6c29dfe8530490f470e6f4b2480b9fd2ae3f10ac
ggbernal <gmo.gbernal>
2021-07-22 17:19:17 -0700

    added sticky navbar

--- menu.js
+++ menu.js
@@ -13,3 +13,18 @@ function show(){
 function close(){
     navLinks.style.top = '-100%';
 }
+
+window.onscroll = function() {addSticky()};
+
+// get the navbar
+const menu = document.querySelector('.prueba')
+const sticky = menu.offsetTop;
+
+function addSticky(){
+    if (window.pageYOffset >= sticky) {
+        menu.classList.add("sticky")
+
+    } else {
+        menu.classList.remove("sticky")
+    }
+}
